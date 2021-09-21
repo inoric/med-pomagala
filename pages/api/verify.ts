8298f32f2a6431fef11b2fb0fe50a7a6163c557a
Dino Pejaković <dino.pejakovic@voxdiversa.hr>
2021-09-21 22:57:48 +0200

    Refractor and fix verify API
    Threw an error on missing SECRET_TOKEN env, just like in b1947c8dac23c06f447ec8ba4fd50a8f0a33e547
    Early-returned a 400 code response on invalid token. 400 is most often used for generic invalid data, "WTF did you send me" cases.
    Early returns help clean up the code flow by avoiding nested IFs.
    
    Got rid of all ifs inside ifs without body:
    ```JS
    if (something)
    if (somethingElse) {
    	...
    }
    ```
    
    It's pretty confusing. Either write it like nested ifs if that's what
    you intended:
    ```JS
    if (something) {
    	if (somethingElse) {
    		...
    	}
    }
    ```
    ...or put it inside the same if block if that's your intention.
    
    Moved getting the date string to getShortDate() because it's way more
    readable, even has the output format explained in the description.
    
    Fixed the problem of sending the response twice. Previously it would try
    to send a 401 after the 200 response every time.

--- pages/api/verify.ts
+++ pages/api/verify.ts
@@ -1,28 +1,50 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import jwt from "jsonwebtoken"
 
+/**
+ * Get the current date as `MMM DD, YYYY`
+ */
+function getShortDate(): string {
+    const now = new Date();
+
+    return now.toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+        year: "numeric"
+    });
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (!process.env.SECRET_TOKEN) {
         throw new Error('Missing SECRET_TOKEN for signing session keys');
     }
-    const token = req.body.token
-    if(jwt.decode(token)!==null)
-    if(jwt.verify(token, process.env.SECRET_TOKEN)){
-        const data = jwt.decode(token)
-        if(typeof data === "object" && data)
-        if(data.date === new Date().toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric" }))
+    const token = req.body.token;
+    const tokenData = jwt.decode(token);
+    if (!tokenData || typeof tokenData !== 'object') {
+        res.status(400).json({
+            data: null,
+            error: 'Invalid token',
+        });
+        return;
+    }
+
+    const isValid = jwt.verify(token, process.env.SECRET_TOKEN);
+    const isNotExpired = tokenData.date === getShortDate();
+
+    if (isValid && isNotExpired) {
         res.status(200).json({
             data: {
-                userId: data.userId,
-                date: data.date,
+                userId: tokenData.userId,
+                date: tokenData.date,
                 token: token,
-                iat: data.iat,
+                iat: tokenData.iat,
             },
             error: false
-        }) 
+        });
+    } else {
+        res.status(401).json({
+            data: null,
+            error: true
+        });
     }
-    res.status(401).json({
-        data: null,
-        error: true
-    }) 
 }

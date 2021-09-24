f10a3062459540962cacdb4592dfb024cfa06b8c
Dino Pejaković <dino.pejakovic@voxdiversa.hr>
2021-09-24 11:05:46 +0200

    Improve and clean up main menu
    Moved menu links into a seperate component to avoid copy-pasting
    everything.
    Added hover styling to links to make it obvious they are clickable.
    Added actual <a> elements so they act like actual links.

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,18 +1,47 @@
 import { Menu, Transition } from '@headlessui/react'
-import { Fragment, useEffect } from 'react'
-import { ArchiveIcon, ClipboardCheckIcon, ClipboardListIcon, CollectionIcon,  LogoutIcon,  MenuIcon, UsersIcon } from '@heroicons/react/outline'
+import { ComponentProps, Fragment, ReactElement } from 'react'
+import { ArchiveIcon, ClipboardCheckIcon, ClipboardListIcon, CollectionIcon, LogoutIcon, MenuIcon, UsersIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
-import { GetServerSideProps } from 'next'
-import jwt from 'jsonwebtoken'
+import { useRouter } from 'next/router'
 
-interface Props {
-    currentPage: string
+interface MenuLinkProps {
+  href: string;
+  title: string;
+  Icon: (iconProps: ComponentProps<'svg'>) => ReactElement;
+  linkClass?: {
+    normal?: string;
+    active?: string;
+  };
+  iconClass?: {
+    normal?: string;
+    active?: string;
+  };
 }
 
+const MenuLink = ({ href, title, Icon, linkClass, iconClass }: MenuLinkProps) => {
+  const router = useRouter();
 
-export default function MainMenu({currentPage}: Props) {
+  const active = router.pathname === href;
+  const iconColor = active
+    ? (iconClass?.active ?? 'text-white-500')
+    : (iconClass?.normal ?? 'text-gray-500');
+  const linkColor = active
+    ? (linkClass?.active ?? 'bg-blue-500 text-white')
+    : (linkClass?.normal ?? 'text-gray-900 hover:bg-gray-100');
 
+  return (
+    <Menu.Item>
+      <Link href={href} passHref>
+        <a className={`group flex rounded-md items-center w-full px-2 py-2 md:text-sm ${linkColor}`}>
+          <Icon className={`w-5 h-5 mr-2 ${iconColor}`} aria-hidden="true" />
+          {title}
+        </a>
+      </Link>
+    </Menu.Item>
+  )
+}
 
+export default function MainMenu() {
   return (
     <div className="w-10 text-right">
       <Menu as="div" className="relative inline-block text-left">
@@ -30,144 +59,25 @@ export default function MainMenu({currentPage}: Props) {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute  right-0 w-60 md:w-52 z-20 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="absolute right-0 w-60 md:w-52 z-20 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="px-1 py-1 ">
-            <Menu.Item>
-                {({ active }) => (
-                  <Link
-                    href="/"><div
-                    className={`${
-                      currentPage==="inventory" ? 'bg-blue-500 text-white' : 'text-gray-900'
-                    } group flex rounded-md items-center w-full px-2 py-2 md:text-sm`}
-                  >
-                    {currentPage==="inventory" ? (
-                      <CollectionIcon
-                        className="w-5 h-5 mr-2 text-white-500"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <CollectionIcon
-                        className="w-5 h-5 mr-2 text-green-500"
-                        aria-hidden="true"
-                      />
-                    )}
-                    Inventar
-                    </div>
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <Link
-                  href="/zaduzenje"><div
-                    className={`${
-                      currentPage==="zaduzenje" ? 'bg-blue-500 text-white' : 'text-gray-900'
-                    } group flex rounded-md items-center w-full px-2 py-2 md:text-sm`}
-                  >
-                    {currentPage==="zaduzenje" ? (
-                      <ClipboardListIcon
-                        className="w-5 h-5 mr-2 text-white-500"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <ClipboardListIcon
-                        className="w-5 h-5 mr-2 text-gray-500"
-                        aria-hidden="true"
-                      />
-                    )}
-                    Zaduženje
-                    </div>
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <Link href="/razduzenje"><div
-                  
-                    className={`${
-                      currentPage==="razduzenje" ? 'bg-blue-500 text-white' : 'text-gray-900'
-                    } group flex rounded-md items-center w-full px-2 py-2 md:text-sm`}
-                  >
-                    {currentPage==="razduzenje" ? (
-                      <ClipboardCheckIcon
-                        className="w-5 h-5 mr-2 text-white-500"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <ClipboardCheckIcon
-                        className="w-5 h-5 mr-2 text-gray-500"
-                        aria-hidden="true"
-                      />
-                    )}
-                    Razduženje
-                    </div>
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <Link href="/users">
-                    <div
-                    className={`${
-                      currentPage==="users" ? 'bg-blue-500 text-white' : 'text-gray-900'
-                    } group flex rounded-md items-center w-full px-2 py-2 md:text-sm`}
-                  >
-                    {currentPage==="users" ? (
-                      <UsersIcon
-                        className="w-5 h-5 mr-2 text-white-500"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <UsersIcon
-                        className="w-5 h-5 mr-2 text-gray-500"
-                        aria-hidden="true"
-                      />
-                    )}
-                    Korisnici
-                    </div>
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <Link href="/arhiva"><div
-                    
-                    className={`${
-                      currentPage==="archive" ? 'bg-blue-500 text-white' : 'text-gray-900'
-                    } group flex rounded-md items-center w-full px-2 py-2 md:text-sm`}
-                  >
-                    {currentPage==="archive" ? (
-                      <ArchiveIcon
-                        className="w-5 h-5 mr-2 text-white-500"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <ArchiveIcon
-                        className="w-5 h-5 mr-2 text-gray-500"
-                        aria-hidden="true"
-                      />
-                    )}
-                    Arhiva
-                    </div>
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <Link href="/login"><div
-                    
-                    className='bg-white text-red-500 group flex rounded-md items-center w-full px-2 py-2 md:text-sm'
-                  >
-                      <LogoutIcon
-                        className="w-5 h-5 mr-2 text-red-500"
-                        aria-hidden="true"
-                      />
-                    Logout
-                    </div>
-                  </Link>
-                )}
-              </Menu.Item>
-              
+              <MenuLink
+                href="/"
+                title="Inventar"
+                Icon={CollectionIcon}
+                iconClass={{ normal: 'text-green-500' }}
+              />
+              <MenuLink href="/zaduzenje" title="Zaduženje" Icon={ClipboardListIcon} />
+              <MenuLink href="/razduzenje" title="Razduženje" Icon={ClipboardCheckIcon} />
+              <MenuLink href="/users" title="Korisnici" Icon={UsersIcon} />
+              <MenuLink href="/arhiva" title="Arhiva" Icon={ArchiveIcon} />
+              <MenuLink
+                href="/login"
+                title="Logout"
+                Icon={LogoutIcon}
+                linkClass={{ normal: 'text-red-500 hover:bg-red-100' }}
+                iconClass={{ normal: 'text-red-500' }}
+              />
             </div>
           </Menu.Items>
         </Transition>
